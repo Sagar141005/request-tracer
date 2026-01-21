7259368270132b672d6cda97c3ba14a1457d3910
Sagar141005 <samsaini5566@gmail.com>
2026-01-21 13:22:03 +0530

    chore: handle cors with client and server on same-origin

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/inspect', (req, res) => {
     console.log(req.headers.origin);
     console.log(req.headers.cookie);
 
+    res.setHeader("Set-Cookie", "session=sess_123; SameSite=None")
+    res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.json({ "origin": req.headers.origin, "cookies": req.headers.cookie });
 });
 
